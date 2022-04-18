1f3fc397a9bf28ce44a333eb96d0a6b7ccfde419
Rafiul789 <89851417+Rafiul789@users.noreply.github.com>
2022-04-18 14:34:13 +0600

    about details added

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,9 +1,15 @@
 import React from 'react';
-
+import image from './image.png'
 const About = () => {
     return (
         <div>
-            <h1>Hello this is my about</h1>
+            <div className="md:flex items-center md:mx-auto   md:justify-evenly"> 
+            
+            <div>  <h1  className="text-4xl"> <span className="text-blue-400 text-6xl"> About  </span> Me </h1>  <h1 className="text-4xl my-5">Rafiul Islam</h1>  <p className="font-semibold"> My goal is to become a web developer.I want to become a full stack web developer.I am ready to achieve my goal. </p>   <br></br> <p className=" font-semibold" >I will practise code daily and watch useful informatinal videos to develop my skill.I will do regular projects which will be important for real life. </p>   </div>
+
+           <div> <img src={image} alt="about"/>   </div> 
+            
+             </div>
         </div>
     );
 };
