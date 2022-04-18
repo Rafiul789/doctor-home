ba18806a46a79a4b6837399cbbb581fee7e4d4c7
Rafiul789 <89851417+Rafiul789@users.noreply.github.com>
2022-04-18 14:14:30 +0600

    notfound page added

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Login from './components/Login/Login';
 
 
 import Navbar from './components/Navbar/Navbar';
+import NotFound from './components/NotFound/NotFound';
 import RequireAuth from './components/RequireAuth/RequireAuth';
 import Signup from './components/Signup/Signup';
 
@@ -17,7 +18,7 @@ function App() {
   return (
     <div >
 <Navbar></Navbar>
-<Routes> <Route path='/' element={<Home></Home>}> </Route> <Route path='/blog' element={<Blog></Blog>}> </Route> <Route path='/checkout' element={ <RequireAuth><Checkout></Checkout>  </RequireAuth>  }></Route> <Route path="/about" element={<About></About>}></Route>    <Route path="/signup" element={<Signup></Signup>}></Route>    <Route path='/login' element={<Login></Login>}></Route>   </Routes>
+<Routes> <Route path='/' element={<Home></Home>}> </Route> <Route path='/blog' element={<Blog></Blog>}> </Route> <Route path='/checkout' element={ <RequireAuth><Checkout></Checkout>  </RequireAuth>  }></Route> <Route path="/about" element={<About></About>}></Route>    <Route path="/signup" element={<Signup></Signup>}></Route>    <Route path='/login' element={<Login></Login>}></Route>  <Route path='*' element={<NotFound></NotFound>} ></Route>    </Routes>
 
         
     </div>
